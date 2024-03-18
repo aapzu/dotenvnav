69884ad5f7e67e9ffd1fd3f12ad52e6dea46890d
Aapeli <aapzu@iki.fi>
2024-03-18 21:47:47 +0200

    Change tests to hopefully pass on CI

--- src/commands/__tests__/listEnvFiles.func.test.ts
+++ src/commands/__tests__/listEnvFiles.func.test.ts
@@ -1,10 +1,13 @@
 import { afterEach } from 'node:test';
+import path from 'node:path';
 
 import mock from 'mock-fs';
 import chalk from 'chalk';
 
 import { runCommand } from '../../tests/testUtils';
 import { createMockLogger } from '../../tests/mockLogger';
+
+const root = path.join(__dirname, '..', '..', '..');
 
 describe('listEnvFiles command', () => {
   afterEach(() => {
@@ -42,19 +45,23 @@ describe('listEnvFiles command', () => {
     expect(info).toEqual(`
 ${chalk.whiteBright('Searching for environment files with pattern .env')}
 ${chalk.whiteBright(
-  '/Users/aapelihaanpuu/workspace/dotenvnav/testProject/foobar/test2/.env\tfoobar__test2__.env',
+  path.join(root, 'testProject/foobar/test2/.env\tfoobar__test2__.env'),
 )}
 ${chalk.whiteBright(
-  '/Users/aapelihaanpuu/workspace/dotenvnav/testProject/foobar/test/.env\tfoobar__test__.env',
+  path.join(root, 'testProject/foobar/test/.env\tfoobar__test__.env'),
 )}
 ${chalk.whiteBright(
-  '/Users/aapelihaanpuu/workspace/dotenvnav/testProject/inner/directory/test2/.env\tinner__directory__test2__.env',
+  path.join(
+    root,
+    'testProject/inner/directory/test2/.env\tinner__directory__test2__.env',
+  ),
 )}
 ${chalk.whiteBright(
-  '/Users/aapelihaanpuu/workspace/dotenvnav/testProject/inner/directory/test/.env\tinner__directory__test__.env',
+  path.join(
+    root,
+    'testProject/inner/directory/test/.env\tinner__directory__test__.env',
+  ),
 )}
-${chalk.whiteBright(
-  '/Users/aapelihaanpuu/workspace/dotenvnav/testProject/.env\troot.env',
-)}`);
+${chalk.whiteBright(path.join(root, 'testProject/.env\troot.env'))}`);
   });
 });
