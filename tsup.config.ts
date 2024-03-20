e5e68939c665c8786525d3bf26871e7b4cc3f5ed
Aapeli Haanpuu <aapzu@iki.fi>
2024-03-20 22:56:05 +0200

    Change to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import type { Options } from 'tsup';
+
+export const tsup: Options = {
+  clean: true,
+  dts: true,
+  format: ['esm'],
+  minify: true,
+  bundle: false,
+  skipNodeModulesBundle: true,
+  entryPoints: ['src/index.ts'],
+  target: 'es2020',
+  outDir: 'dist',
+  entry: ['src/**/*.ts'], //include all files under src
+};
