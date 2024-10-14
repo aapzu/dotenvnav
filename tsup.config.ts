ea08eebf3859cb09b666daa49db9067ce45869bc
Aapeli <aapzu@iki.fi>
2024-10-14 19:38:13 +0300

    Try and fix builds

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,10 +4,9 @@ export const tsup: Options = {
   clean: true,
   dts: true,
   format: ['esm'],
-  bundle: true,
-  skipNodeModulesBundle: true,
   entryPoints: ['src/index.ts'],
-  target: 'es2020',
+  target: 'es2022',
   outDir: 'dist',
-  entry: ['src/**/*.ts'], //include all files under src
+  splitting: false,
+  entry: ['src', '!src/**/__tests__', '!src/testUtils'],
 };
